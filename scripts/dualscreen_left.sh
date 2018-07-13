b9c0c83cb35075776fadba08303df33a75cd6a36
Accacio <raccacio@poli.ufrj.br>
2018-07-13 16:18:41 +0200

    changer order of dualscreen workspaces

--- scripts/dualscreen_left.sh
+++ scripts/dualscreen_left.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+i3-msg "workspace 10, move workspace to output eDP"
+i3-msg "workspace 9, move workspace to output eDP"
+i3-msg "workspace 8, move workspace to output eDP"
+i3-msg "workspace 7, move workspace to output eDP"
+i3-msg "workspace 6, move workspace to output eDP"
+
+
+
 i3-msg "workspace 5, move workspace to output DisplayPort-1"
 i3-msg "workspace 4, move workspace to output DisplayPort-1"
 i3-msg "workspace 3, move workspace to output DisplayPort-1"
 i3-msg "workspace 2, move workspace to output DisplayPort-1"
 i3-msg "workspace 1, move workspace to output DisplayPort-1"
 
-i3-msg "workspace 10, move workspace to output eDP"
-i3-msg "workspace 9, move workspace to output eDP"
-i3-msg "workspace 8, move workspace to output eDP"
-i3-msg "workspace 7, move workspace to output eDP"
-i3-msg "workspace 6, move workspace to output eDP"
+
