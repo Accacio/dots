ebaa350380e6a824c5593312bf85e58a0cbf4de9
Accacio <raccacio@poli.ufrj.br>
2018-09-15 22:07:05 -0300

    Organisation and Audio shortcuts

--- bash/.bashrc.d/alias.bash
+++ bash/.bashrc.d/alias.bash
@@ -22,22 +22,19 @@ fi
 
 alias myproxy='PW=`openssl aes-256-cbc -d -in ~/.pw`; PROXY="http://$PW@$proxyip"; export http_proxy=$PROXY; export https_proxy=$PROXY; export ftp_proxy=$PROXY; git config --global --replace-all http.proxy http://$PW@$proxyip'
 alias cls="printf '\ec'"
-# some more ls aliases
 alias l='ls -CF'
 alias ll='ls -lF'
 alias lla='ls -alF'
 alias la='ls -A'
 alias gitgui='git gui &'
 alias clc='clear'
-alias cls="printf '\ec'"
 alias u='xrandr -o normal'
 alias d='xrandr -o inverted'
 alias lsip='ifconfig | grep "inet "'
 alias ..='cd ..'
 alias ....='cd ../..'
+
 alias bcd="cd -"
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias gcd='cd ~/git/ && cd'
