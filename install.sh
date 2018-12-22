1aaf7140fbab87778fc12352d33024f394529e28
Accacio <raccacio@poli.ufrj.br>
2018-12-22 16:32:19 -0200

    adds configuration to fonts, zathura, ranger etc

--- install.sh
+++ install.sh
@@ -8,10 +8,16 @@ stow -v -d configs/ -t ~/ \
      mpd/      \
      ncmpcpp/  \
      xinitrc/  \
-     xournal/
+     xournal/  \
+     fonts
+
+stow -v -d configs/ -t ~/.config/ \
+     mpv/      \
+     newsboat/ \
+     zathura/  \
+     ranger/
 
 # install custom .desktop files
 stow -v -d configs/ -t ~/.local/share/applications/ applications/
-
 
 # inside .config
