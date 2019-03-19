016fbd5a0275a54f2bda7fa73e5818ee8c7e83f9
Accacio <raccacio@poli.ufrj.br>
2019-03-18 23:36:00 -0300

    clean aliases

--- configs/bash/.bashrc.d/alias.bash
+++ configs/bash/.bashrc.d/alias.bash
@@ -1,22 +1,33 @@
 
-# enable color support of ls and also add handy aliases
-# if [ -x /usr/bin/dircolors ]; then
-#     test -r $HOME/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-#     alias ls='ls --color=auto'
-#     #alias dir='dir --color=auto'
-#     #alias vdir='vdir --color=auto'
-
-#     alias grep='grep --color=auto'
-#     alias fgrep='fgrep --color=auto'
-#     alias egrep='egrep --color=auto'
-# fi
-
-
-
-# To encrypt
-# openssl aes-256-cbc -e -in file_to_encrypt -out encrypted_file
-
 [ -f "$HOME/.proxy_address" ] && . "$HOME/.proxy_address"
+
+export LESS='-R'
+export LESSOPEN='|pygmentize -g %s'
+
+
+function proxyOn {
+    PW=`openssl aes-256-cbc -d -in $HOME/.pw`;
+    PROXY="http://$PW@$proxyIp";
+      export http_proxy=$PROXY; 
+      export https_proxy=$PROXY;
+      export ftp_proxy=$PROXY; 
+      git config --global --replace-all http.proxy http://$PW@$proxyIp
+}
+
+function proxyOff {
+    sed "s/.*proxy.*//" $HOME/.gitconfig > $HOME/.gitconfig
+}
+
+function emc {
+    emacsclient -c $1 &
+    disown
+}
+
+function emct {
+    emacsclient -ct $1 &
+    disown
+}
+
 function qrCli {
     temp=$(mktemp)
     qrencode $1 -o $temp
@@ -25,11 +36,8 @@ function qrCli {
     rm $temp
 }
 
-export LESS='-R'
-export LESSOPEN='|pygmentize -g %s'
 
-alias myproxy='PW=`openssl aes-256-cbc -d -in $HOME/.pw`; PROXY="http://$PW@$proxyip"; export http_proxy=$PROXY; export https_proxy=$PROXY; export ftp_proxy=$PROXY; git config --global --replace-all http.proxy http://$PW@$proxyip' \
-      cls="printf '\ec'" \
+alias cls="printf '\ec'" \
       l='ls -CFbh' \
       lls='ls -CFbh --color|less' \
       l.="ls -dbh .??*" \
@@ -42,37 +50,31 @@ alias myproxy='PW=`openssl aes-256-cbc -d -in $HOME/.pw`; PROXY="http://$PW@$pro
       llals='ls -AlFbh --color | less' \
       la='ls -Abh' \
       lals='ls -Abh --color | less' \
-      gitgui='git gui &' \
       clc='clear' \
       lsip='ifconfig | grep "inet "' \
       ..='cd ..' \
       ....='cd ../..' \
       ......='cd ../../..' \
       emacsd="emacs -daemon" \
-      emc="emacsclient -c" \
-      emct="emacsclient -t" \
-      sc="sc-im"
+      sc="sc-im" \
       accSite="surf 192.168.1.140:4000 &" \
       jsl="cd $HOME/blog/;jekyll serve --livereload" \
       music='tmux new-session "tmux source-file $HOME/dots/configs/ncmpcpp/ncmpcpp/tmux_session"' \
-      wpp="surf web.whatsapp.com &" \
       addBooksRecursive='find -name "*.pdf" -print0 |xargs -0 -I file cp file ~/books/TCC'
 
 # alias u='xrandr -o normal'
 # alias d='xrandr -o inverted'
 
 
-function vim {
-ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-socket=/tmp/nvim-$ID
-nvr --servername $socket --remote $1
-}
+# function vim {
+# ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+# socket=/tmp/nvim-$ID
+# nvr --servername $socket --remote $1
+# }
+
 function - {
     cd -
 }
-
-
-
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
@@ -84,21 +86,31 @@ function addBook {
     # done
 }
 
-
-
 function gcd {
 	cd "$HOME/git/$(ls -a ~/git/|xargs -n1|fzy)"
 }
 
-alias wpp="surf web.whatsapp.com &"
+alias wpp="surf web.whatsapp.com & disown; exit"
 
 function loadZ {
+    if [ -f $1 ]
+       then
+       echo Insert network path
+       return
+    fi
     echo "username"
-    read -s user
+    read user
     echo "password"
     read -s pass
-    sudo mount -t cifs -o username=$user,password=$pass,uid=1000,gid=1000 //BRRIOWD009009/confs /home/accacio/Z/
+    sudo mount -t cifs -o username=$user,password=$pass,uid=1000,gid=1000 $1 /home/accacio/Z/
 }
 
 alias unloadZ="cd; sudo umount ~/Z"
 
+function encrypt {
+    openssl aes-256-cbc -e -in $1 -out $2
+}
+
+function decrypt {
+    openssl aes-256-cbc -d -in $1 -out $2
+}
