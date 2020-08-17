3f08331cef7cdd98ccf626c91626f9bf44347b7a
Accacio <raccacio@poli.ufrj.br>
2020-08-17 13:44:46 +0200

    add minimap

--- configs/doom/doom/init.el
+++ configs/doom/doom/init.el
@@ -25,6 +25,7 @@
        (ivy +childframe)               ; a search engine for love and life
 
        :ui
+       minimap
        deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        ;; doom-dashboard    ; a nifty splash screen for Emacs
