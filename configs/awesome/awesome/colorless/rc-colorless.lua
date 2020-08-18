21d3bbcfbeafe7838915e8cf332b5ce5531cfea4
Accacio <raccacio@poli.ufrj.br>
2020-08-18 14:56:01 +0200

    hide all titlebars

--- configs/awesome/awesome/colorless/rc-colorless.lua
+++ configs/awesome/awesome/colorless/rc-colorless.lua
@@ -189,28 +189,28 @@ local gears = require("gears")
 -- turn titlebar on when client is floating
 -------------------------------------------------------------------------------
 client.connect_signal("property::floating", function(c)
-  if c.floating then
-    awful.titlebar.show(c)
-  else
+  -- if c.floating then
+  --   awful.titlebar.show(c)
+  -- else
     awful.titlebar.hide(c)
-  end
+  -- end
 end)
 
 client.connect_signal("manage", function(c)
-    if c.floating or c.first_tag.layout.name == "floating" then
-        awful.titlebar.show(c)
-    else
+    -- if c.floating or c.first_tag.layout.name == "floating" then
+    --     awful.titlebar.show(c)
+    -- else
         awful.titlebar.hide(c)
-    end
+    -- end
 end)
 tag.connect_signal("property::layout", function(t)
     local clients = t:clients()
     for k,c in pairs(clients) do
-        if c.floating or c.first_tag.layout.name == "floating" then
-            awful.titlebar.show(c)
-        else
+        -- if c.floating or c.first_tag.layout.name == "floating" then
+        --     awful.titlebar.show(c)
+        -- else
             awful.titlebar.hide(c)
-        end
+        -- end
     end
 end)
 
