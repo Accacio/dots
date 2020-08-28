f3dcda0e2773095ae606048e11b05823b0586de0
Accacio <raccacio@poli.ufrj.br>
2020-08-28 14:24:00 +0200

    exclude html files and .git

--- configs/bash/.bashrc.d/alias.bash
+++ configs/bash/.bashrc.d/alias.bash
@@ -152,13 +152,13 @@ function decrypt {
     openssl aes-256-cbc -d -in $1 -out $2
 }
 
-alias org2dbox="rclone --exclude .orgids --exclude org-roam.db -l -u copy ~/org dbox:org"
-alias dbox2org="rclone --exclude .orgids --exclude org-roam.db -l -u copy dbox:org ~/org"
+alias org2dbox="rclone --exclude '*.html' --exclude .git/ --exclude .orgids --exclude org-roam.db -l -u copy ~/org dbox:org"
+alias dbox2org="rclone --exclude '*.html' --exclude .git/ --exclude .orgids --exclude org-roam.db -l -u copy dbox:org ~/org"
 alias these2dbox="rclone -l -u copy ~/these dbox:these"
 alias dbox2these="rclone -l -u copy dbox:these ~/these"
 
-alias org2dboxSync="rclone --exclude org-roam.db -l sync ~/org dbox:org"
-alias dbox2orgSync="rclone --exclude .orgids --exclude org-roam.db -l sync dbox:org ~/org"
+alias org2dboxSync="rclone --exclude '*.html' --exclude .git/ --exclude org-roam.db -l sync ~/org dbox:org"
+alias dbox2orgSync="rclone --exclude '*.html' --exclude .git/ --exclude .orgids --exclude org-roam.db -l sync dbox:org ~/org"
 alias these2dboxSync="rclone -l sync ~/these dbox:these"
 alias dbox2theseSync="rclone -l sync dbox:these ~/these"
 
