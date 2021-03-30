7cfa1ed23096fa2589a88842e38270b2bff6fd81
Accacio <raccacio@poli.ufrj.br>
2021-03-30 16:47:37 +0200

    org-roam-server config to serve images

--- configs/doom/doom/config.el
+++ configs/doom/doom/config.el
@@ -657,7 +657,20 @@ MYTAG"
            :unnarrowed t))))
 
   (org-roam-bibtex-mode)
-(use-package! org-roam-server)
+(use-package! org-roam-server
+  :config
+  (setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 8080
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files t
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20))
+
 ;; org-journal
 (use-package! org-journal
   :bind
