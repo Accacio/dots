c249a6e8ecdf8559bf1f1c1cef588b0bddfc855f
Accacio <rafael.accacio.nogueira@gmail.com>
2024-04-22 09:39:36 +0200

    remove alert

--- configs/bash/.bashrc.d/alias.bash
+++ configs/bash/.bashrc.d/alias.bash
@@ -87,8 +87,6 @@ function - {
     cd -
 }
 
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
 [ -f "$HOME/bin/createShortcuts" ] && "$HOME/bin/createShortcuts" && . $HOME/.shortcuts
 
 function gcd {
