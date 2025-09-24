2c375a96d1314873db860616df5ce993d36aa5b3
Accacio <rafael.accacio.nogueira@gmail.com>
2025-09-24 09:39:38 +0200

    indentation

--- configs/awesome/awesome/rc.lua
+++ configs/awesome/awesome/rc.lua
@@ -6,9 +6,9 @@
 -----------------------------------------------------------------------------------------------------------------------
 local rc = "colorless.rc-colorless"
 
---local rc = "color.red.rc-red"
---local rc = "color.blue.rc-blue"
---local rc = "color.orange.rc-orange"
+-- local rc = "color.red.rc-red"
+-- local rc = "color.blue.rc-blue"
+-- local rc = "color.orange.rc-orange"
 -- local rc = "color.green.rc-green"
 
 -- local rc = "shade.ruby.rc-ruby"
